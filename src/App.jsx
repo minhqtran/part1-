06e856ffe30a8d9bef771f50b487c504eedaa3f7
Minh Tran <kunnietran159@gmail.com>
2024-04-14 14:03:23 +0700

    Passing state - to child component

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,28 @@
-import React from "react";
+import { useState } from "react";
 
-const Header = (props) => {
-  console.log(props);
-  return (
-    <>
-      <p> {props.course.name}</p>
-    </>
-  );
+const Display = (props) => {
+  return <div>{props.counter}</div>;
 };
 
-const Content = (props) => {
-  console.log(props);
-  return (
-    <>
-      <p>
-        {props.parts[0].name} {props.parts[0].exercises}
-      </p>
-      <p>
-        {props.parts[1].name} {props.parts[1].exercises}
-      </p>
-      <p>
-        {props.parts[2].name} {props.parts[2].exercises}
-      </p>
-    </>
-  );
-};
-
-const Total = (props) => {
-  console.log(props);
-  const total = props.course.parts
-    .map((part) => part.exercises)
-    .reduce((sum, exercises) => sum + exercises, 0);
-  return <p>Total excercises: {total}</p>;
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
 };
 
 const App = () => {
-  const course = {
-    name: "Half  Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
+
+  console.log("rendering...", counter);
   return (
-    <>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total course={course} />
-    </>
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={decreaseByOne} text="minus" />
+      <Button onClick={setToZero} text="zero" />
+    </div>
   );
 };
 
