abee74f18011e08a1db9e9992d955f63f7533a2e
Minh Tran <kunnietran159@gmail.com>
2024-04-20 03:23:41 +0700

    Finish 1.10

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,76 @@
 import { useState } from "react";
 
-const Display = (props) => {
-  return <div>{props.counter}</div>;
-};
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
-const Button = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>;
+const StatisticLine = ({ text, value }) => (
+  <p>
+    {text} : {value}
+  </p>
+);
+
+const Statistics = ({ good, bad, neutral, average, all }) => {
+  if (all === 0) {
+    return (
+      <>
+        <h2>Statistics</h2>
+        <p>No feed back Given</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h2>Statistics</h2>
+      <StatisticLine text="Good" value={good} />
+      <StatisticLine text="Neutral" value={neutral} />
+      <StatisticLine text="Bad" value={bad} />
+      <StatisticLine text="Total" value={all} />
+      <StatisticLine text="Average" value={average / all} />
+      <StatisticLine text="Positive" value={good / all} />
+    </>
+  );
 };
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  console.log("rendering with counter value", counter);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
 
-  const increaseByOne = () => {
-    console.log("increasing, value before", counter);
-    setCounter(counter + 1);
+  const handleGood = () => {
+    console.log("good selected");
+    setGood(good + 1);
+    setAll(all + 1);
+    setAverage(average + 1);
+  };
+  const handleNeutral = () => {
+    console.log("neutral selected");
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  };
+  const handleBad = () => {
+    console.log("bad selected");
+    setBad(bad + 1);
+    setAll(all + 1);
+    setAverage(average - 1);
   };
 
-  const decreaseByOne = () => {
-    console.log("decreasing, value before", value);
-    setCounter(counter - 1);
-  };
-
-  const setToZero = () => {
-    console.log("resetting to zero, value before", counter);
-    setCounter(0);
-  };
-
-  console.log("rendering...", counter);
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={decreaseByOne} text="minus" />
-      <Button onClick={setToZero} text="zero" />
+      <h2>Give Feedback</h2>
+      <Button handleClick={handleGood} text={"Good"} />
+      <Button handleClick={handleNeutral} text={"Neutral"} />
+      <Button handleClick={handleBad} text={"Bad"} />
+      <Statistics
+        good={good}
+        bad={bad}
+        neutral={neutral}
+        all={all}
+        average={average}
+      />
     </div>
   );
 };
