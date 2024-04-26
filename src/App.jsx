2a92bf91f35ae6de8005f3b4777a1e90aa0e5c52
Minh Tran <kunnietran159@gmail.com>
2024-04-26 19:01:00 +0700

    finish the excercise of part1

--- src/App.jsx
+++ src/App.jsx
@@ -4,82 +4,37 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const StatisticLine = ({ text, value }) => (
-  <tr>
-    <td>{text}</td>
-    <td>{value}</td>
-  </tr>
-);
-
-const Statistic = ({ text, value }) => (
-  <table>
-    <tbody>
-      <StatisticLine text={text} value={value} />
-    </tbody>
-  </table>
-);
-
-const Statistics = ({ good, bad, neutral, average, all }) => {
-  if (all === 0) {
-    return (
-      <>
-        <h2>Statistics</h2>
-        <p>No feed back Given</p>
-      </>
-    );
-  }
-
-  return (
-    <>
-      <h2>Statistics</h2>
-      <Statistic text="Good" value={good} />
-      <Statistic text="Neutral" value={neutral} />
-      <Statistic text="Bad" value={bad} />
-      <Statistic text="Total" value={all} />
-      <Statistic text="Average" value={average / all} />
-      <Statistic text="Positive" value={good / all} />
-    </>
-  );
-};
-
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0);
-  const [average, setAverage] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-  const handleGood = () => {
-    console.log("good selected");
-    setGood(good + 1);
-    setAll(all + 1);
-    setAverage(average + 1);
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const randomAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
   };
-  const handleNeutral = () => {
-    console.log("neutral selected");
-    setNeutral(neutral + 1);
-    setAll(all + 1);
+  const vote = () => {
+    const newVotes = [...votes];
+    newVotes[selected]++;
+    setVotes(newVotes);
   };
-  const handleBad = () => {
-    console.log("bad selected");
-    setBad(bad + 1);
-    setAll(all + 1);
-    setAverage(average - 1);
-  };
-
   return (
     <div>
-      <h2>Give Feedback</h2>
-      <Button handleClick={handleGood} text={"Good"} />
-      <Button handleClick={handleNeutral} text={"Neutral"} />
-      <Button handleClick={handleBad} text={"Bad"} />
-      <Statistics
-        good={good}
-        bad={bad}
-        neutral={neutral}
-        all={all}
-        average={average}
-      />
+      <h1>Anecdote of the day</h1>
+      <div>{anecdotes[selected]}</div>
+      <div>has {votes[selected]} votes </div>
+      <Button handleClick={vote} text="vote" />
+      <Button handleClick={randomAnecdote} text="Generate random anecdote" />
+      <h1>Anecdote with most vote</h1>
+      <div>{anecdotes[votes.indexOf(Math.max(...votes))]}</div>
     </div>
   );
 };
